cb6fc202cb593f64a47adfabc961306cb9c6f521
dgroumpos <dngroumpos@hotmail.com>
2024-02-22 22:21:01 +0200

    Setup sqlalchemy

--- fkcommerce/core/__init__.py
+++ fkcommerce/core/__init__.py
@@ -2,12 +2,20 @@ import os
 
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
+
+database = SQLAlchemy()
 
 def create_app(config_type = os.getenv("CONFIG_TYPE")):
     app = Flask(__name__)
 
     app.config.from_object(config_type)
 
+    initialize_extensions(app)
+
     return app
+
+def initialize_extensions(app):
+    database.init_app(app)
